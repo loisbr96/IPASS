f46ffbea93965f9427b43597039d2314d2d157cc
loisbr96 <lois.brandsema@student.hu.nl>
2018-06-15 13:10:11 +0200

    Fixed the date to string in de DaoImpl from slaapplek

--- src/main/java/nl/hu/ipass/persistence/SlaapplekDaoImpl.java
+++ src/main/java/nl/hu/ipass/persistence/SlaapplekDaoImpl.java
@@ -10,7 +10,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SlaapplekDaoImpl extends mysqlBaseDao implements SlaapplekDao {
@@ -19,10 +23,18 @@ public class SlaapplekDaoImpl extends mysqlBaseDao implements SlaapplekDao {
     public Slaapplek save(Slaapplek slaapplek) {
         try(Connection con = super.getConnection()){
             PreparedStatement prep = con.prepareStatement("INSERT INTO slaapplek(datum, huisId, studentId) VALUES(?,?,?)");
-            prep.setString(1,slaapplek.getDatum() );
-            prep.setInt(2,slaapplek.getHuis().getId() );
-            prep.setInt(3,slaapplek.getStudent().getId() );
-            prep.execute();
+            try{
+                DateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+                Date datum =  df.parse(slaapplek.getDatum());
+                java.sql.Date sqlDatum = new java.sql.Date(datum.getTime());
+                prep.setDate(1, sqlDatum );
+                prep.setInt(2,slaapplek.getHuis().getId() );
+                prep.setInt(3,slaapplek.getStudent().getId() );
+                prep.execute();
+
+            } catch (ParseException pe){
+                System.out.println(pe);
+            }
             return slaapplek;
         } catch (SQLException e){
             System.out.println(e);
@@ -34,10 +46,17 @@ public class SlaapplekDaoImpl extends mysqlBaseDao implements SlaapplekDao {
     public boolean update(Slaapplek slaapplek) {
         try(Connection con = super.getConnection()) {
             PreparedStatement prep = con.prepareStatement("UPDATE slaapplek SET datum = ?, huisId = ?, studentId = ? WHERE id = ? ");
-            prep.setString(1,slaapplek.getDatum() );
-            prep.setInt(2,slaapplek.getHuis().getId() );
-            prep.setInt(3,slaapplek.getStudent().getId() );
-            prep.setInt(4,slaapplek.getId() );
+            try {
+                DateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+                Date datum = df.parse(slaapplek.getDatum());
+                java.sql.Date sqlDatum = new java.sql.Date(datum.getTime());
+                prep.setDate(1, sqlDatum);
+                prep.setInt(2, slaapplek.getHuis().getId());
+                prep.setInt(3, slaapplek.getStudent().getId());
+                prep.setInt(4, slaapplek.getId());
+            } catch (ParseException pe){
+                System.out.println(pe);
+            }
 
             prep.execute();
             return true;
@@ -81,25 +100,25 @@ public class SlaapplekDaoImpl extends mysqlBaseDao implements SlaapplekDao {
         return results;
     }
 
-    @Override
-    public Slaapplek findByDatum(String datum) {
-        return selectSlaapplek("SELECT * FROM slaapplek WHERE datum = STR_TO_DATE('" + datum + "', '%d-%m-%Y')").get(0);
-    }
+//    @Override
+//    public Slaapplek findByDatum(String datum) {
+//        return selectSlaapplek("SELECT * FROM slaapplek WHERE datum = STR_TO_DATE('" + datum + "', '%d-%m-%Y')").get(0);
+//    }
 
-    @Override
-    public List<Slaapplek> findByHuis(Huis huis) {
-        return selectSlaapplek("SELECT * FROM slaapplek WHERE huisId = '" + huis.getId() + "'");
-    }
+//    @Override
+//    public List<Slaapplek> findByHuis(Huis huis) {
+//        return selectSlaapplek("SELECT * FROM slaapplek WHERE huisId = '" + huis.getId() + "'");
+//    }
 
     @Override
     public Slaapplek findById(int id) {
         return selectSlaapplek("SELECT * FROM slaapplek WHERE id = '" + id + "'").get(0);
     }
 
-    @Override
-    public List<Slaapplek> findByStudent(Student student) {
-        return selectSlaapplek("SELECT * FROM slaapplek WHERE studentId = '" + student.getId() + "'");
-    }
+//    @Override
+//    public List<Slaapplek> findByStudent(Student student) {
+//        return selectSlaapplek("SELECT * FROM slaapplek WHERE studentId = '" + student.getId() + "'");
+//    }
 
     @Override
     public List<Slaapplek> findByHuisAndDatum(Huis huis, String datum) {
